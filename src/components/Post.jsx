ee5fedb7d88bb3cca3322e10bc58101ea0166f89
AmmaraGillani <ammara.gillani79@gmail.com>
2024-06-08 11:25:32 +0500

    Add icons component to the post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { HiDotsHorizontal } from 'react-icons/hi';
 import Link from 'next/link';
+import Icons from './Icons';
 
 export default function Post({post, id}) {
   return (
     <div className='flex p-3 border-b border-gray-200'>
-        <img src={post?.profileImg} alt='user-img' className='h-11 w-11 rounded-full mr-4' />
+        <img src={post?.profileImg} alt='user-img' className='h-11 w-11 rounded-full mr-4 hover:bg-gray-50' />
         <div className='flex-1'>
             <div className='flex items-center justify-between'>
                 <div className='flex items-center space-x-1 whitespace-nowrap'>
@@ -24,6 +25,7 @@ export default function Post({post, id}) {
                 className='rounded-2xl mr-2 cursor-pointer'
                 />
             </Link>
+            <Icons />
         </div>
     </div>
   )
